9d2930820bb319fdfb848318d0590d9efaf7554d
jingzhang <jingzhang@zhaoxin.com>
2022-05-02 17:10:49 +0800

    dajing-20220502 build a enviorment in ZX PC.
    Signed-off-by: jingzhang <jingzhang@zhaoxin.com>

--- qemu.sh
+++ qemu.sh
@@ -1,1 +1,3 @@
+rm -f /hda-contents/DajingApp.efi
+cp /home/jing/Desktop/edk2/Build/Shell/DEBUG_GCC5/X64/ShellPkg/Application/MyEdk2/DajingApp/DajingApp/DEBUG/DajingApp.efi ./hda-contents
 qemu-system-x86_64 -pflash ./hda-contents/bios.bin -hda fat:rw:hda-contents -net none
